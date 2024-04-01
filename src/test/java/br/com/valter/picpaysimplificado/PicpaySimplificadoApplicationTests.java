46846cb2edff18796cab67e864bc280bc06853b9
valter.junior <valter_123@hotmail.com>
2024-04-01 11:37:41 -0300

    feat: commit inicial

---
+++ src/test/java/br/com/valter/picpaysimplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valter.picpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
