3fb5d6b895c0dcdb772b6d936a200a4e7fba1fa7
valter.junior <valter_123@hotmail.com>
2024-04-08 22:58:47 -0300

    refact: ajustado config do kafka

--- src/main/java/br/com/valter/picpaysimplificado/notificacao/infra/KafkaConfig.java
+++ src/main/java/br/com/valter/picpaysimplificado/notificacao/infra/KafkaConfig.java
@@ -1,30 +1,15 @@
 package br.com.valter.picpaysimplificado.notificacao.infra;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import java.util.List;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
 @Configuration
 public class KafkaConfig {
-    @Value("${app.kafka.notificacao.groupId}")
-    private String groupId;
-
 
     @Bean
-    @Primary
-    public KafkaProperties kafkaProperties(){
-        KafkaProperties props = new KafkaProperties();
-        props.setBootstrapServers(List.of("localhost:9092"));
-        props.getConsumer().setGroupId(groupId);
-        props.getConsumer().setAutoOffsetReset("earliest");
-        props.getProducer().setValueSerializer(org.apache.kafka.common.serialization.StringSerializer.class);
-        props.getConsumer().setValueDeserializer(org.apache.kafka.common.serialization.StringDeserializer.class);
-        props.getConsumer().setEnableAutoCommit(false);
-
-        return props;
+    public RecordMessageConverter converter(){
+        return new StringJsonMessageConverter();
     }
 }
